92c1eb9b6c611bf3c7a562c24623741d4dcd2bb0
Martin McGrane <mmcgr@users.noreply.github.com>
2026-09-02 12:53:49 +1000

    test fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
+using Test
 using SafeTestsets
-
-# --- TESTS ---
-@safetestset "SPICE Utils" begin include("spice_utils.jl") end
-@safetestset "NBP" begin include("nbp.jl") end
-@safetestset "3BP" begin include("3bp.jl") end
-@safetestset "Document tests" begin include("doctests.jl") end
-@safetestset "Sensitivity" begin include("sensitivity.jl") end
-@safetestset "Aqua" begin include("aqua.jl") end
+@testset "OrbitalTrajectories.jl" begin
+    @safetestset "SPICE Utils" begin include("spice_utils.jl") end
+    @safetestset "NBP" begin include("nbp.jl") end
+    @safetestset "3BP" begin include("3bp.jl") end
+    @safetestset "Document tests" begin include("doctests.jl") end
+    @safetestset "Sensitivity" begin include("sensitivity.jl") end
+    @safetestset "Aqua" begin include("aqua.jl") end
+end
