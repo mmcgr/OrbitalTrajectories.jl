e83e97dcf936d63532923acee3cb916501c85db0
Martin McGrane <mmcgr@users.noreply.github.com>
2026-09-11 13:29:43 +1000

    Drop cse

--- src/dynamics/dynamical_models.jl
+++ src/dynamics/dynamical_models.jl
@@ -2,11 +2,8 @@
 # GENERIC MODEL METHODS #
 #-----------------------#
 
-import ..cse
-
-# wrap_code: perform common substring elimination to improve performance of the resulting models.
 @doc """Generic constructor for a DynamicalModel's underlying ODEFunctions."""
-function (T::Type{<:Abstract_ModelODEFunctions})(args...; wrap_code=(cse, cse), kwargs...)
+function (T::Type{<:Abstract_ModelODEFunctions})(args...; kwargs...)
     _ode = ODESystem(T, args...; kwargs...)
 
     # Reduce high-order system to 1st-order (re-ordering to keep the original equations first)
@@ -20,8 +17,8 @@ function (T::Type{<:Abstract_ModelODEFunctions})(args...; wrap_code=(cse, cse), 
 
     # Generate the functions
     # TODO: Add support for tgrad (need to define derivative(get_pos) for EphemerisNBP)
-    ode_f = ODEFunction(ode; jac=true, tgrad=false, eval_expression=false, eval_module=@__MODULE__, wrap_code)
-    ode_stm_f = STM_ODEFunction(ode, ode_f; wrap_code)
+    ode_f = ODEFunction(ode; jac=true, tgrad=false, eval_expression=false, eval_module=@__MODULE__)
+    ode_stm_f = STM_ODEFunction(ode, ode_f)
     T(ode, ode_f, ode_stm_f)
 end
 
