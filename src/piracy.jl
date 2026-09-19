aa6ef5a8f156c4cefb0b67f70495ed64e3423e48
Martin McGrane <mmcgr@users.noreply.github.com>
2026-09-19 19:50:18 +1000

    Disable piracy

--- src/piracy.jl
+++ src/piracy.jl
@@ -5,9 +5,9 @@ using ForwardDiff: ForwardDiff, Dual
 # TODO: Check which of these are still in use and/or necessary.
 
 # Avoid Plots compatibility issue with Quantify
-Base.occursin(s::Any, q::Quantity) = occursin(s, repr(q))
-Base.pointer(q::Quantity) = pointer(repr(q))
+# Base.occursin(s::Any, q::Quantity) = occursin(s, repr(q))
+# Base.pointer(q::Quantity) = pointer(repr(q))
 
-# Ensure that ForwardDiff values can get sent to C calls
-# Base.unsafe_convert(T::Type{<:Any}, x::Dual) = T(ForwardDiff.value.(x))
-Base.unsafe_convert(::Type{Cwstring}, x::Dual) = Cwstring(ForwardDiff.value.(x))
+# # Ensure that ForwardDiff values can get sent to C calls
+# # Base.unsafe_convert(T::Type{<:Any}, x::Dual) = T(ForwardDiff.value.(x))
+# Base.unsafe_convert(::Type{Cwstring}, x::Dual) = Cwstring(ForwardDiff.value.(x))
