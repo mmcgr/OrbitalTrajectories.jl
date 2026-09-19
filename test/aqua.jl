287693db8a7e551aba180aec482a4d91c27f0f0e
Martin McGrane <mmcgr@users.noreply.github.com>
2026-09-19 13:47:11 +1000

    Extend Aqua tests

--- test/aqua.jl
+++ test/aqua.jl
@@ -3,5 +3,15 @@ using OrbitalTrajectories
 using Test
 
 @testset "Stale dependencies" begin
-    Aqua.test_stale_deps(OrbitalTrajectories)
+    #Aqua.test_stale_deps(OrbitalTrajectories)
+    Aqua.test_all(
+        OrbitalTrajectories;
+        # TODO: Are the piracies still needed?
+        # treat_as_own = [
+        #     LinearAlgebra.norm,
+        #     Base.occursin,
+        #     Base.pointer,
+        #     Base.unsafe_convert,
+        # ],
+    )
 end
